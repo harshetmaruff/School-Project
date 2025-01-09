4d41578a22118dd90ccef10b31f477491e0daf92
Harshet Maruff <43926773+harshetmaruff@users.noreply.github.com>
2025-01-09 14:52:26 +0530

    Add SignUp Page

---
+++ frontend/src/SignUp.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function SignUp() {
+    
+  useEffect(() => {
+    fetchString()
+  }, []);
+
+  const fetchString = async () => {
+    const response = await fetch("http://127.0.0.1:8000/lost", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+    });
+    if (response.ok) {
+      const data = await response.json();
+       setMessage(data.msg)
+       console.log(data)
+    }
+  }
+
+  return (
+    <>
+      <div className='LoginDiv'>
+        <div className='LoginElement'>
+          <div style={{textAlign: 'center'}}>
+              Registration
+          </div>
+          <form>
+            <input type="text" placeholder='Enter username' style={{marginTop: '1rem'}}/>
+            <br />
+            <input type="text" placeholder='Enter email' style={{marginTop: '1rem'}}/>
+            <br />
+            <input type="text" placeholder='Enter password' style={{marginTop: '1rem'}}/>
+            <br />
+            <button type='submit'>Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SignUp
