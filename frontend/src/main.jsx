cdcd8c92978bb8c57b1886e335810635b03b08a3
Harshet Maruff <43926773+harshetmaruff@users.noreply.github.com>
2025-01-09 14:51:45 +0530

    Add React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from './App.jsx'
+import SignUp from './SignUp.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+        <Route index element={<SignUp/>}/>
+        <Route path='login' element={<App/>}/>
+    </Routes>
+  </BrowserRouter>,
 )
