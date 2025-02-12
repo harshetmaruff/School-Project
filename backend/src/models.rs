2dd7f8b1151e0b4caad4cc3e3a9b2e8fbcaad2c4
Harshet Maruff <43926773+harshetmaruff@users.noreply.github.com>
2025-02-12 20:39:09 +0530

    Add Accounts Table and Structure
    Add Accounts table in diesel migration
    Add Structs in models.rs

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,3 +1,5 @@
+use actix_web::cookie::time::Date;
+use diesel::sql_types::Decimal;
 use serde::{Deserialize, Serialize};
 
 use crate::schema::users;
@@ -9,6 +11,8 @@ pub struct Data {
     pub msg: String
 }
 
+
+//-------------- Models for Users -----------------
 #[derive(Debug, Insertable, Deserialize)]
 #[diesel(table_name = users)]
 pub struct NewUser {
@@ -24,3 +28,40 @@ pub struct User {
     pub email: String,
     pub password_hash: String
 }
+//--------------------------------------------------
+
+
+//---------------- Models for Accounts -------------
+pub struct ExchangeRate {
+    pub id: i32,
+    pub base_currency: String,
+    pub target_currency: String,
+    pub rate: f32,
+    pub effective_date: Date
+}
+
+pub struct FinancialYear {
+    pub id: i32,
+    pub name: String,
+    pub start_date: Date,
+    pub end_date: Date,
+    pub status: bool,
+    pub created_at: Date,
+    pub updated_at: Date
+}
+
+pub struct Journal {
+    pub id: i32,
+    pub voucher_no: String,
+    pub ledger_id: String,
+    pub transaction_type_id: i32,
+    pub transaction_date: Date,
+    pub description: String,
+    pub debit: f32,
+    pub credit: f32,
+    pub currency_code: String,
+    pub created_at: Date,
+    pub updated_at: Date
+}
+
+//--------------------------------------------------
--- backend/src/ops.rs
+++ backend/src/ops.rs
@@ -1,1 +1,2 @@
 pub mod user_data;
+pub mod accounts;
