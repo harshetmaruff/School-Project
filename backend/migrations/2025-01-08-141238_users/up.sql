cb384c413f8fa534777bce04298dfd49576c6a15
Harshet Maruff <43926773+harshetmaruff@users.noreply.github.com>
2025-02-13 16:19:38 +0530

    Add Sample User and user roles

--- backend/migrations/2025-01-08-141238_users/up.sql
+++ backend/migrations/2025-01-08-141238_users/up.sql
@@ -1,7 +1,24 @@
 -- Your SQL goes here
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,               -- Auto-incrementing primary key
+    id SERIAL PRIMARY KEY,                -- Auto-incrementing primary key
     username VARCHAR(50) NOT NULL UNIQUE, -- Username with a max length of 50, must be unique
     email VARCHAR(100) NOT NULL UNIQUE,   -- Email with a max length of 100, must be unique
     password_hash TEXT NOT NULL          -- Password hash for storing encrypted passwords
+    user_role_id INT,
+    CONSTRAINT fk_user_role FOREIGN KEY (user_role_id) REFERENCES user_role(id) ON DELETE SET NULL
 );
+
+CREATE TABLE user_role (
+    id SERIAL PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL UNIQUE
+)
+
+INSERT INTO user_role (id, role_name) VALUES
+(1, "Admin"),
+(2, "Cashier"),
+(3, "Accountant");
+
+INSERT INTO users (email, user_role_id, username, password_hash)
+("admin@example.com", 1, "admin", "admin123"),
+("cashier@example.com", 2, "cashier", "cashier123"),
+("accountant@example.com", 3,  "accountant", "accountant123");
