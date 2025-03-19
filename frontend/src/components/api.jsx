7df2f2a8b6123e1bd10847a30b1abde60da1b8d8
Harshet Maruff <43926773+harshetmaruff@users.noreply.github.com>
2025-03-19 13:41:37 +0530

    Add API Links In React

--- frontend/src/components/api.jsx
+++ frontend/src/components/api.jsx
@@ -1,22 +1,109 @@
+import { useNavigate } from "react-router";
+
 export const apiurl = "http://127.0.0.1:8080"
 
 
-export const getUserToken = async (user_id) => {
+export const getUserToken = async (uname, pwd) => {
     const Method = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        mode: 'cors'
+        mode: 'cors',
+        body: JSON.stringify({
+             username:  uname,
+             password: pwd
+            })
     }
 
-    const request = await fetch(apiurl + "/login", Method)
+    try {
+        const request = await fetch(apiurl + "/login", Method);
 
-    const data = request.json()
+        const data = await request.json().catch(() => ({}));
 
-    if(request.ok) {
-        data.then(value => {
-            console.log(value)
-        })
+        if (request.status === 401) {
+            alert(`Error 401: ${data.error || 'Unauthorized. Incorrect username or password.'}`);
+            return false;
+        }
+
+        if (request.ok) {
+            if (data.token) {
+                console.log("Token:", data.token);
+                localStorage.setItem('token', data.token);
+                return true
+            }
+        } else {
+            console.log(`Error: ${request.status} - ${data.error || 'Unknown error'}`);
+            return false
+        }
+
+    } catch (error) {
+        console.error("Network error:", error.message);
+        return false
+    }
+}
+
+export const getCurrency = async () => {
+    const navigate = useNavigate();
+
+    const Method = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+    }
+
+    try {
+        const request = await fetch(apiurl + "/accounts/currency", Method);
+
+        const data = await request.json().catch(() => ({}));
+
+        if (request.status == 401) {
+            console.log(`Error 401: ${data.error || 'Unauthorized. Incorrect username or password.'}`);
+            navigate("/")
+        }
+
+        if (request.ok) {
+            data.then(value => {
+                return value
+            })
+        }
+
+    } catch (error) {
+        console.error("Network error:", error.message);
+    }
+}
+
+export const postCurrency = async (Data) => {
+    const navigate = useNavigate();
+
+    const Method = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        body: JSON.stringify(Data)
+    }
+
+    try {
+        const request = await fetch(apiurl + "/accounts/currency", Method)
+
+        const data = await request.json().catch(() => ({}));
+
+        if (request.status = 401) {
+            console.log(`Error 401: ${data.error || 'Unauthorized. Incorrect username or password.'}`);
+            navigate("/");
+        }
+
+        if (request.ok) {
+            data.then(value => {
+                return value
+            })
+        }
+    }
+    catch (error) {
+        console.error("Network error:", error.message);
     }
 }
